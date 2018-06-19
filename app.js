79f8786ffc0556d38320449701dfe2ce3a648003
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-06-18 20:18:10 -0700

    Basic wireframe Started.

---
+++ app.js
@@ -0,0 +1,3 @@
+const apiKey = 'RGAPI-846886ba-070b-401a-8108-01e87d06c8e5'
+
+//on submit send the search to Riot api and return a list of players with their mastery levels, profile levels and their player icon

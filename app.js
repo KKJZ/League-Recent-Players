ba4ce9ced19eb2fc43f662345dd54814518f9272
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-06-22 21:00:41 -0700

    wire frame done js began

--- app.js
+++ app.js
@@ -1,11 +1,64 @@
-const FORTNITE_ENDPOINT = 'https://api.fortnitetracker.com/v1/profile/{platform}/{epic-nickname}'
-const apiKey = '54456bf0-d031-45f1-b056-cf9105de92c5'
+const RIOT_ENDPOINT = 'https://na1.api.riotgames.com';
+const apiKey = 'RGAPI-46060c3b-046e-4fbf-9fd1-b5ead5a9d24a';
 
-//search though the fortnite api and return stats about your player on which ever system you told the search
+//search though the LoL api and return stats about your player on which ever system you told the search
 
-//make results list change actice 
-function handleButtonList () {
-	$('div.list-group').on('click', function(e) {
+//handle the submit button 
+function handleSubmit () {
+	$('div.list-group').on('submit', function(e) {
+		e.preventDefault();
+		console.log(this);
+		$(this).css(active);
+	});
+}
+
+//first make a call to the endpoint with what the user entered
+function getName (name, callback) {
+	const nameURL = RIOT_ENDPOINT + `/lol/summoner/v3/summoners/by-name/${name}?api_key=${apiKey}`;
+	console.log(nameURL);
+	const settings = {
+		url: nameURL,
+		type: 'GET',
+		dataType:'json',
+		success: callback,
+		error: forFailure
+		};
+	$.ajax(settings);
+}
+
+//render stats to screen
+function getStats (name) {
+	const playerId = name.id;
+	const playerName = name.name;
+	const summonerLevel = name.summonerLevel;
+	const profileIcon = name.profileIconId;
+	renderName(playerName, summonerLevel, profileIcon);
+}
+
+function getIcon (icon, callback) {
+	const iconURL = RIOT_ENDPOINT + `/lol/static-data/v3/profile-icons?api_key=${apiKey}`
+	const settings = {
+		url: iconURL,
+		type: 'GET',
+		dataType: 'json',
+		success: callback,
+		error: forFailure
+	};
+	$.ajax(settings);
+}
+
+function renderName (name, level, icon) {
+	getIcon(icon, test);
+}
+
+//If we received an error
+function forFailure (name) {
+	console.log(name);
+}
+
+//handle the submit button 
+function handleSubmit () {
+	$('div.list-group').on('submit', function(e) {
 		e.preventDefault();
 		console.log(this);
 		$(this).css(active);
@@ -13,7 +66,13 @@ function handleButtonList () {
 }
 
 function onLoad () {
-	handleButtonList();
+	handleSubmit();
+	getName("Kyle226", getStats);
 }
 
+function test (test) {
+	const ugh = test.data[0];
+	console.log(ugh);
+}
 onLoad();
+

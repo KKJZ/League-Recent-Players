b47dc188ee679396c4477e962a2d43ebbca4fe4b
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-07-19 09:46:29 -0700

    Updated Key

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const RIOT_ENDPOINT = 'https://na1.api.riotgames.com';
-const apiKey = 'RGAPI-bdae3754-3fe6-4bd2-a087-b35eb9794aab';
+const apiKey = 'RGAPI-e7b89900-8caa-488d-b28f-62806d151276';
 
 //add the id for the icon + .png
 const iconURL = `http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/`;
